aac1cbd84116f3b6d56c43b63fb04987e58136c0
enzonjagi <amosndungo@gmail.com>
2023-05-01 15:48:07 +0300

    Changed up the look and feel of the admin portal

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    """We use this to specify which fields 
+    the Django admin should display
+    """
+
+    model = User
+    # Limit the Django admin: to just dsiplay the username
+    fields = ["username"]
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
