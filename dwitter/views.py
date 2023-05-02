6f5f39ff8a7d537cdff8ec68530c6bdae4f62ed8
enzonjagi <amosndungo@gmail.com>
2023-05-02 11:50:34 +0300

    Created a Profile list template and view to generate a list of profiles the app has on the patter: dwitter/profile_list/

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 def dashboard(request):
     """Renders the base.html template on the app's dashboard"""
 
     return render(request, "base.html")
+
+def profile_list(request):
+    """Creates a profile list from the Profiles created in the DB"""
+
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(
+        request,
+        "dwitter/profile_list.html",
+        {"profiles": profiles},
+    )
