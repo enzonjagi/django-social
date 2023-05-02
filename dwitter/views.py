6baa5bf3228d0402bcb20a07f45e8ea2e97e06ff
enzonjagi <amosndungo@gmail.com>
2023-05-02 11:32:04 +0300

    Created a base template for the frontend and styled it with Bulma CSS

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dashboard(request):
+    """Renders the base.html template on the app's dashboard"""
+
+    return render(request, "base.html")
