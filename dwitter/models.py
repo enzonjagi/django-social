9a1004a59b9999b8d3c7de321b110d437dced49d
enzonjagi <amosndungo@gmail.com>
2023-05-02 10:13:47 +0300

    An update to the USer and Profile: Once you create a SUer, a profile is automatically created and the user follows themselves automatically to view his/her own posts

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -18,3 +19,17 @@ class Profile(models.Model):
         """A description of the User who owns this profile"""
 
         return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    """Should help with automatic Profile and User creation and association"""
+
+    # checks if user was created successfully, 
+    # then creates a Profile instance for it
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+# Create a Profile for each new User
+post_save.connect(create_profile, sender=User)
