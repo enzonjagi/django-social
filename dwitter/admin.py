bab6bdfac3db3d80be0fcbf78d6b14da3d1a0879
enzonjagi <amosndungo@gmail.com>
2023-05-02 09:54:37 +0300

    Creating Profile model for the App and attaching user models to profile models

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile
 
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    """An inline Admin Profile registration
+    
+    Makes it easier to connect the User and Profile Models
+    """
+
+    model = Profile
+
 class UserAdmin(admin.ModelAdmin):
     """We use this to specify which fields 
     the Django admin should display
@@ -10,6 +19,7 @@ class UserAdmin(admin.ModelAdmin):
     model = User
     # Limit the Django admin: to just dsiplay the username
     fields = ["username"]
+    inlines = [ProfileInline]
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
--- dwitter/models.py
+++ dwitter/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    """Creates a Profile Model for Users on the App"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True,
+    )
+
+
+    def __str__(self):
+        """A description of the User who owns this profile"""
+
+        return self.user.username
