997bdece6b143c38c9fe5ce1b037fdea0c006eba
enzonjagi <amosndungo@gmail.com>
2023-05-03 10:09:19 +0300

    Added a functionality to create Dweets on the Backend: But are not displayed on the frontend at this stage

--- dwitter/models.py
+++ dwitter/models.py
@@ -44,3 +44,10 @@ class Dweet(models.Model):
     )
     body = models.CharField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"({self.created_at:%Y-%m-%d %H:%M}): "
+            f"{self.body[:100]}..."
+        )
